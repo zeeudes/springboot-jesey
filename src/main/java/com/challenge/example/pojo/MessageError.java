6ec65da1fca6b606fee19cafb34ecffaaf64f923
José Eudes de Souza Júnior <eudes.souza@ufpe.br>
2019-03-17 20:21:38 -0300

    Configuracão das versões ds bibliotecas modificadas para a RELEASE. Inclusão do pojo MessageError

--- src/main/java/com/challenge/example/pojo/MessageError.java
+++ src/main/java/com/challenge/example/pojo/MessageError.java
@@ -1,12 +1,12 @@
 package com.challenge.example.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
 public class MessageError {
     private String message;
     private Integer errorCode;
